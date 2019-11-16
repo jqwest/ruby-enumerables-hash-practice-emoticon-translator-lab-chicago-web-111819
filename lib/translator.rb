445bfd3ec5e7b22a59f510b8632457f7c442a24f
john L <johnleavell@gmail.com>
2019-11-16 22:07:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,13 @@ def load_library(file_path)
     "get_meaning" => {},
     "get_emoticon" => {}
   }
+
+  emoticons.each do |key,value|
+    new_hash['get_emoticon'][value[0]] = emoticons[key][1]
+    new_hash['get_meaning'][value[1]] = key
+  end
+
+  new_hash
 end
 
 def get_japanese_emoticon
